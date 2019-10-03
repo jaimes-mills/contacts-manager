0f346ef15ea29f52e3d233dd078d42f84b97c22e
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-03 14:18:14 -0500

    final commit

--- src/Welcome.java
+++ src/Welcome.java
@@ -6,11 +6,11 @@ public class Welcome extends TrackContacts {
 
 
 public void welcomeDisplay(){
-    System.out.println("1. View contacts.\n" +
-            "2. Add a new contact.\n" +
-            "3. Search a contact by name.\n" +
-            "4. Delete an existing contact.\n" +
-            "5. Exit.\n" +
+    System.out.println("1. View contacts\n" +
+            "2. Add a new contact\n" +
+            "3. Search a contact by name\n" +
+            "4. Delete an existing contact\n" +
+            "5. Exit\n" +
             "Enter an option (1, 2, 3, 4 or 5):");
 }
 
